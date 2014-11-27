471d44a7d44da57a19bd6d958a236cd97ef5c8f0
walrusone <russell_j_wall@hotmail.com>
2014-11-27 11:34:29 -0330

    Update to Version 2.0
    Update to Version 2.0

--- HeadLeaderBoards/src/main/java/com/headleaderboards/headleaderboards/SignUpdater.java
+++ HeadLeaderBoards/src/main/java/com/headleaderboards/headleaderboards/SignUpdater.java
@@ -29,7 +29,7 @@ public class SignUpdater implements Callable<Object> {
     	switch (lineColor) {
     	case "black": return '0';
     	case "dark blue": return '1';
-    	case "dark gren": return '2';
+    	case "dark green": return '2';
     	case "dark aqua": return '3';
     	case "dark red": return '4';
     	case "dark purple": return  '5';
@@ -43,6 +43,22 @@ public class SignUpdater implements Callable<Object> {
     	case "light purple": return 'd';
     	case "yellow": return 'e';
     	case "white": return 'f';
+    	case "0": return '0';
+    	case "1": return '1';
+    	case "2": return '2';
+    	case "3": return '3';
+    	case "4": return '4';
+    	case "5": return  '5';
+    	case "6": return '6';
+    	case "7": return '7';
+    	case "8": return '8';
+    	case "9": return '9';
+    	case "a": return 'a';
+    	case "b": return 'b';
+    	case "c": return 'c';
+    	case "d": return 'd';
+    	case "e": return 'e';
+    	case "f": return 'f';
     	default: return '0';
        	}
     }
